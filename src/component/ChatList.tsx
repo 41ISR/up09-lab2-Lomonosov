aa0fd3f2c02dd47b4d924952e70562b353ec9043
Tepa556 <132324241+Tepa556@users.noreply.github.com>
2025-02-12 22:56:40 +0600

    над немног доработать логику чата в main

--- src/component/ChatList.tsx
+++ src/component/ChatList.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStoreChat from '../lib/StoreUserChat';
 import useStoreUser from '../lib/StoreUserData'; 
 
 const ChatList: React.FC = () => {
     const { chats } = useStoreChat();
     const { userIdStore } = useStoreUser();
+    const [activeChatId, setActiveChatId] = useState<string | null>(null);
+
+    const handleChatClick = (chatId: string) => {
+        setActiveChatId(chatId);
+        console.log(`Чат с ID ${chatId} активирован`);
+    };
 
     return (
         <div>
             <h2>Список Чатов</h2>
             <ul>
                 {chats.map((chat) => {
-
                     const secondUser = chat.messages.find(msg => msg.userId !== userIdStore?.id);
 
                     return (
-                        <li key={chat.id} className="sidebar-list-item">
+                        <li 
+                            key={chat.id} 
+                            className={`sidebar-list-item ${activeChatId === chat.id ? 'active' : ''}`} 
+                            onClick={() => handleChatClick(chat.id)}
+                        >
                             {secondUser && (
                                 <>
                                     <div className="user-avatar"></div>
