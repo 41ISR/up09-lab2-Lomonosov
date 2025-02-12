05acf2ec8ee268bc1ac9795c62dcd6a07947deb3
Tepa556 <132324241+Tepa556@users.noreply.github.com>
2025-02-12 19:47:07 +0600

    чуток поработал с чатом

--- src/component/ChatList.tsx
+++ src/component/ChatList.tsx
@@ -1,12 +1,33 @@
-const ChatList=()=>{
-    
-    return(
+import React from 'react';
+import useStoreChat from '../lib/StoreUserChat';
+import useStoreUser from '../lib/StoreUserData'; 
+
+const ChatList: React.FC = () => {
+    const { chats } = useStoreChat();
+    const { userIdStore } = useStoreUser();
+
+    return (
         <div>
-            <div className="sidebar-list-item">
-                <div className="user-avatar"></div>
-                Чатик
-            </div>
+            <h2>Список Чатов</h2>
+            <ul>
+                {chats.map((chat) => {
+
+                    const secondUser = chat.messages.find(msg => msg.userId !== userIdStore?.id);
+
+                    return (
+                        <li key={chat.id} className="sidebar-list-item">
+                            {secondUser && (
+                                <>
+                                    <div className="user-avatar"></div>
+                                    <strong>{secondUser.userId}</strong> 
+                                </>
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
-    )
-}
-export default ChatList
+    );
+};
+
+export default ChatList;
